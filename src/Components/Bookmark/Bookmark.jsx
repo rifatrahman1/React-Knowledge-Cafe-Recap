5cdca386b5ea7a95ea309cda69ed81aa953005bb
rifatrahman1 <ahsan30rifat@gmail.com>
2025-01-05 07:14:15 +0600

    added to mark as read components

--- src/Components/Bookmark/Bookmark.jsx
+++ src/Components/Bookmark/Bookmark.jsx
@@ -3,8 +3,11 @@ import PropTypes from 'prop-types';
 const Bookmark = ({bookmark}) => {
     const {title} = bookmark;
     return (
-        <div className='bg-white rounded-lg'>
-            <h3 className='text-[18px] font-semibold p-5  rounded-lg space-y-4'>{title}</h3>
+        <div >
+            
+           <div className='bg-white rounded-lg'>
+           <h3 className='text-[18px] font-semibold p-5  rounded-lg space-y-4'>{title}</h3>
+           </div>
         </div>
     );
 };
--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -4,8 +4,11 @@ import Bookmark from '../Bookmark/Bookmark';
 const Bookmarks = ({bookmarks}) => {
     return (
         <div className="col-span-1">
-            <div className='bg-[#1111110D] p-8 space-y-4 rounded-xl'>
-            <h1 className='text-2xl font-bold'>Booksmarked Blog: {bookmarks.length}</h1>
+            <div className='bg-[#6047EC1A] px-12 py-5 rounded-xl border border-[#6047EC]'>
+                <h1 className='text-2xl font-bold text-[#6047EC]'>Spent time on read : </h1>
+            </div>
+            <div className='bg-[#1111110D] p-8 space-y-4 rounded-xl mt-6'>
+            <h1 className='text-2xl font-bold'>Booksmarked Blog : {bookmarks.length}</h1>
             {
                 bookmarks.map((bookmark, idx) => <Bookmark key={idx} bookmark={bookmark}></Bookmark>)
             }
